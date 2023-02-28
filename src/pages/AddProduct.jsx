3a1bcade0da62a3b50f1759e25b8992ebc43c228
qirall79 <belfatmi79@gmail.com>
2023-02-28 11:01:39 +0100

    Add footer to addproduct page

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, Navigate } from "react-router-dom";
+import Footer from "../components/Footer";
 import addProduct from "../utils/addProduct";
 import fetchProducts from "../utils/fetchProducts";
 
@@ -134,179 +135,191 @@ const AddProduct = ({ products, setProducts }) => {
   }
 
   return (
-    <div className="">
-      <div className="p-10 bg-slate-500 flex justify-between items-center">
-        <h1 className="text-3xl">Product Add</h1>
-        <ul className="flex gap-10">
-          <li className="font-semibold">
-            <button onClick={handleSubmit}>Save</button>
-          </li>
-          <li className="font-semibold">
-            <Link to={"/"}>Cancel</Link>
-          </li>
-        </ul>
-      </div>
+    <div className="w-full min-h-[90vh] h-full flex flex-col justify-between">
+      <div>
+        <div className="p-10 bg-slate-500 flex justify-between items-center">
+          <h1 className="text-3xl">Product Add</h1>
+          <ul className="flex gap-10">
+            <li className="font-semibold">
+              <button onClick={handleSubmit}>Save</button>
+            </li>
+            <li className="font-semibold">
+              <Link to={"/"}>Cancel</Link>
+            </li>
+          </ul>
+        </div>
 
-      <div className="p-10">
-        <form
-          className="flex flex-col gap-8 w-1/2"
-          id="product_form"
-          method="post"
-        >
-          <div className="form-group flex flex-col gap-3">
-            <div className="flex gap-5 justify-between">
-              <label htmlFor="sku">SKU</label>
-              <input
-                className="w-[80%] h-8 outline-none px-2"
-                type="text"
-                name="sku"
-                id="sku"
-              />
+        <div className="p-10">
+          <form
+            className="flex flex-col gap-8 w-1/2"
+            id="product_form"
+            method="post"
+          >
+            <div className="form-group flex flex-col gap-3">
+              <div className="flex gap-5 justify-between">
+                <label htmlFor="sku">SKU</label>
+                <input
+                  className="w-[80%] h-8 outline-none px-2"
+                  type="text"
+                  name="sku"
+                  id="sku"
+                />
+              </div>
+              <p
+                id="sku-error"
+                className="text-red-600 text-sm font-medium"
+              ></p>
             </div>
-            <p id="sku-error" className="text-red-600 text-sm font-medium"></p>
-          </div>
 
-          <div className="form-group flex flex-col gap-3">
-            <div className="flex gap-5 justify-between">
-              <label htmlFor="name">Name</label>
-              <input
-                className="w-[80%] h-8 outline-none px-2"
-                type="text"
-                name="name"
-                id="name"
-              />
+            <div className="form-group flex flex-col gap-3">
+              <div className="flex gap-5 justify-between">
+                <label htmlFor="name">Name</label>
+                <input
+                  className="w-[80%] h-8 outline-none px-2"
+                  type="text"
+                  name="name"
+                  id="name"
+                />
+              </div>
+              <p
+                id="name-error"
+                className="text-red-600 text-sm font-medium"
+              ></p>
             </div>
-            <p id="name-error" className="text-red-600 text-sm font-medium"></p>
-          </div>
 
-          <div className="form-group flex flex-col gap-3">
-            <div className="w-full flex gap-5 justify-between">
-              <label htmlFor="price">Price ($)</label>
-              <input
-                className="w-[80%] h-8 outline-none px-2"
-                type="number"
-                min="0"
-                name="price"
-                id="price"
-              />
+            <div className="form-group flex flex-col gap-3">
+              <div className="w-full flex gap-5 justify-between">
+                <label htmlFor="price">Price ($)</label>
+                <input
+                  className="w-[80%] h-8 outline-none px-2"
+                  type="number"
+                  min="0"
+                  name="price"
+                  id="price"
+                />
+              </div>
+              <p
+                id="price-error"
+                className="text-red-600 text-sm font-medium"
+              ></p>
             </div>
-            <p
-              id="price-error"
-              className="text-red-600 text-sm font-medium"
-            ></p>
-          </div>
-          <div className="form-group flex gap-5 justify-between">
-            <label htmlFor="productType">Type Switcher</label>
-            <select
-              onChange={handleTypeChange}
-              className="w-[80%] outline-none"
-              name="type"
-              id="productType"
+            <div className="form-group flex gap-5 justify-between">
+              <label htmlFor="productType">Type Switcher</label>
+              <select
+                onChange={handleTypeChange}
+                className="w-[80%] outline-none"
+                name="type"
+                id="productType"
+              >
+                <option value="book" id="Book">
+                  Book
+                </option>
+                <option value="dvd" id="DVD">
+                  DVD
+                </option>
+                <option value="furniture" id="Furniture">
+                  Furniture
+                </option>
+              </select>
+            </div>
+
+            <div
+              className={`form-group ${
+                productType === "dvd" ? "flex" : "hidden"
+              } flex-col gap-3 justify-between`}
             >
-              <option value="book" id="Book">
-                Book
-              </option>
-              <option value="dvd" id="DVD">
-                DVD
-              </option>
-              <option value="furniture" id="Furniture">
-                Furniture
-              </option>
-            </select>
-          </div>
+              <div className="flex gap-5 justify-between">
+                <label htmlFor="size">Size (MB)</label>
+                <input
+                  className="w-[80%] h-8 outline-none px-2"
+                  type="number"
+                  min="0"
+                  name="size"
+                  id="size"
+                />
+              </div>
 
-          <div
-            className={`form-group ${
-              productType === "dvd" ? "flex" : "hidden"
-            } flex-col gap-3 justify-between`}
-          >
-            <div className="flex gap-5 justify-between">
-              <label htmlFor="size">Size (MB)</label>
-              <input
-                className="w-[80%] h-8 outline-none px-2"
-                type="number"
-                min="0"
-                name="size"
-                id="size"
-              />
+              <p
+                id="size-error"
+                className="text-red-600 text-sm font-medium"
+              ></p>
+              <p className="text-sm font-medium text-green-600 ">
+                *Please specify the size of your DVD disk
+              </p>
             </div>
 
-            <p id="size-error" className="text-red-600 text-sm font-medium"></p>
-            <p className="text-sm font-medium text-green-600 ">
-              *Please specify the size of your DVD disk
-            </p>
-          </div>
-
-          <div
-            className={`form-group ${
-              productType === "book" ? "flex" : "hidden"
-            } flex-col gap-3 justify-between`}
-          >
-            <div className="flex gap-5 justify-between">
-              <label htmlFor="weight">Weight (KG)</label>
-              <input
-                className="w-[80%] h-8 outline-none px-2"
-                type="number"
-                min="0"
-                name="weight"
-                id="weight"
-              />
-            </div>
-            <p
-              id="weight-error"
-              className="text-red-600 text-sm font-medium"
-            ></p>
-            <p className="text-sm font-medium text-green-600 ">
-              *Please specify the weight of your book
-            </p>
-          </div>
-
-          <div
-            className={`form-group ${
-              productType === "furniture" ? "flex" : "hidden"
-            } flex-col gap-8`}
-          >
-            <div className="w-full flex gap-5 justify-between">
-              <label htmlFor="height">Height</label>
-              <input
-                className="w-[80%] h-8 outline-none px-2"
-                type="number"
-                min="0"
-                name="height"
-                id="height"
-              />
-            </div>
-            <div className="w-full flex gap-5 justify-between">
-              <label htmlFor="width">Width</label>
-              <input
-                className="w-[80%] h-8 outline-none px-2"
-                type="number"
-                min="0"
-                name="width"
-                id="width"
-              />
+            <div
+              className={`form-group ${
+                productType === "book" ? "flex" : "hidden"
+              } flex-col gap-3 justify-between`}
+            >
+              <div className="flex gap-5 justify-between">
+                <label htmlFor="weight">Weight (KG)</label>
+                <input
+                  className="w-[80%] h-8 outline-none px-2"
+                  type="number"
+                  min="0"
+                  name="weight"
+                  id="weight"
+                />
+              </div>
+              <p
+                id="weight-error"
+                className="text-red-600 text-sm font-medium"
+              ></p>
+              <p className="text-sm font-medium text-green-600 ">
+                *Please specify the weight of your book
+              </p>
             </div>
 
-            <div className="w-full flex gap-5 justify-between">
-              <label htmlFor="length">Length</label>
-              <input
-                className="w-[80%] h-8 outline-none px-2"
-                type="number"
-                min="0"
-                name="length"
-                id="length"
-              />
+            <div
+              className={`form-group ${
+                productType === "furniture" ? "flex" : "hidden"
+              } flex-col gap-8`}
+            >
+              <div className="w-full flex gap-5 justify-between">
+                <label htmlFor="height">Height</label>
+                <input
+                  className="w-[80%] h-8 outline-none px-2"
+                  type="number"
+                  min="0"
+                  name="height"
+                  id="height"
+                />
+              </div>
+              <div className="w-full flex gap-5 justify-between">
+                <label htmlFor="width">Width</label>
+                <input
+                  className="w-[80%] h-8 outline-none px-2"
+                  type="number"
+                  min="0"
+                  name="width"
+                  id="width"
+                />
+              </div>
+
+              <div className="w-full flex gap-5 justify-between">
+                <label htmlFor="length">Length</label>
+                <input
+                  className="w-[80%] h-8 outline-none px-2"
+                  type="number"
+                  min="0"
+                  name="length"
+                  id="length"
+                />
+              </div>
+              <p
+                id="dimension-error"
+                className="text-red-600 text-sm font-medium"
+              ></p>
+              <p className="text-sm font-medium text-green-600 ">
+                *Please specify the dimensions of your furniture
+              </p>
             </div>
-            <p
-              id="dimension-error"
-              className="text-red-600 text-sm font-medium"
-            ></p>
-            <p className="text-sm font-medium text-green-600 ">
-              *Please specify the dimensions of your furniture
-            </p>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
